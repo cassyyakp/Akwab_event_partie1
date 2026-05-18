97e88c9e19724ccc33de1ff85157687689adfb65
johannorens <johannorens009@gmail.com>
2026-05-18 21:29:10 +0000

    request login register utilisateur et evenement

---
+++ akwab_api/app/Http/Requests/StoreUtilisateurRequest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Contracts\Validation\ValidationRule;
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreUtilisateurRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'nom'          => 'required|string|max:255',
+            'prenoms'      => 'required|string|max:255',
+            'email'        => 'required|email|unique:utilisateurs,email',
+            'telephone'    => 'required|string|max:20',
+            'mot_de_passe' => 'required|string|min:8',
+            'id_role'      => 'required|exists:roles,id_role',
+        ];
+    }
+}
